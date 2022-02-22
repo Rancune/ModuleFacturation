a5fab6a667fd0631dbf7548a27266e0e13d3c198
Briqn <pitet.brian@gmail.com>
2022-02-22 15:15:00 +0100

    Mise en place du projet

---
+++ src/com/myfacturation/client/clientInfo.java
@@ -0,0 +1,10 @@
+package com.myfacturation.client;
+
+public class clientInfo {
+    public String firstName;
+    public String lastName;
+    public String adress;
+    public String mail;
+    public String tel;
+
+}
---
+++ src/com/myfacturation/presta/prestaInfo.java
@@ -0,0 +1,9 @@
+package com.myfacturation.presta;
+
+public class prestaInfo {
+    public String name;
+    public String price;
+    public String quantity;
+
+
+}
